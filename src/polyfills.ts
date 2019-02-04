2f5e71f989c736cdf1061de9ef59509595c2a97a
Michael Karén <michael.karen@gmail.com>
2019-02-04 15:16:30 +0100

    first commit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,6 +57,8 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+import 'core-js/es7/reflect';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
